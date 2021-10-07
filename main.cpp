9d2cefcb221ca74a7c72504f6b397acc59621e08
Piotr Rogalski <progalski89@gmail.com>
2021-10-07 22:32:36 +0200

    Main features working

--- main.cpp
+++ main.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
+#include <utility>
 #include "unique_ptr.hpp"
 
 class SimpleClass {
 public:
     SimpleClass() {
-        std::cout << "c'tor\n";
+        std::cout << "SimpleClass c'tor\n";
     }
     ~SimpleClass() {
-        std::cout << "d'tor\n";
+        std::cout << "SimpleClass d'tor\n";
     }
 
     void simpleMethod() {
-        std::cout << "Simple Method\n";
+        std::cout << "SimpleMethod\n";
     }
 };
 
 int main() {
-    pr::unique_ptr up(new SimpleClass);
+    pr::unique_ptr up{new SimpleClass};
     up->simpleMethod();
     (*up).simpleMethod();
+    // pr::unique_ptr new_instance = up;
+    // pr::unique_ptr new_instance{up};
+    pr::unique_ptr<SimpleClass> new_instance{nullptr};
+    new_instance = std::move(up);
+    pr::unique_ptr even_newer_instance{std::move(new_instance)};
     return 0;
 }
--- unique_ptr.hpp
+++ unique_ptr.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+
 namespace pr {
 
 template <class T>
@@ -13,8 +15,19 @@ public:
         ptr_ = nullptr;
     }
 
-    unique_ptr(const unique_ptr& other) = delete;
+    unique_ptr& operator=(unique_ptr&& other) noexcept {
+        this->ptr_ = other.realase();
+        std::cout << "unique ptr move operator=\n";
+        return *this;
+    }
+
+    unique_ptr(unique_ptr&& other) noexcept {
+        this->ptr_ = other.realase();
+        std::cout << "unique ptr move c'tor\n";
+    }
+
     unique_ptr& operator=(const unique_ptr& other) = delete;
+    unique_ptr(const unique_ptr& other) = delete;
 
     T* operator->() const noexcept {
         return this->get();
@@ -24,17 +37,17 @@ public:
         return *this->get();
     }
 
-    T* get() const  {
+    T* get() const noexcept {
         return ptr_;
     }
 
-    T* realase() {
+    T* realase() noexcept {
         T* temporary = ptr_;
         ptr_ = nullptr;
         return temporary;
     }
 
-    void reset(T* other) {
+    void reset(T* other) noexcept {
         delete ptr_;
         ptr_ = other;
     }
